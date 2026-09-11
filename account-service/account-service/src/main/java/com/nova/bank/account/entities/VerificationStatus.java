18834247de57be664711bedb9f91700bd776af32
vinaybackend <vinay.backend.coder@gmail.com>
2026-09-11 19:16:18 +0530

    Add account and transaction services

---
+++ account-service/account-service/src/main/java/com/nova/bank/account/entities/VerificationStatus.java
@@ -0,0 +1,10 @@
+package com.nova.bank.account.entities;
+
+public enum VerificationStatus {
+
+    PENDING,
+    UNDER_REVIEW,
+    APPROVED,
+    REJECTED,
+    EXPIRED
+}
---
+++ transaction-service/transaction-service/src/test/java/com/nova/bank/transaction/TransactionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nova.bank.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
